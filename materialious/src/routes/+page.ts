6943ca491cb37ab46b90c812ea7e046e898cc8f9
WardPearce <wardpearce@pm.me>
2024-04-03 03:15:00 +1300

    Fix popular page being disabled

--- materialious/src/routes/+page.ts
+++ materialious/src/routes/+page.ts
@@ -1,7 +1,19 @@
-import { getPopular } from '$lib/Api/index.js';
+import { getPopular, getTrending } from '$lib/Api/index.js';
+import { error } from '@sveltejs/kit';
 
-export async function load({ params }) {
+export async function load() {
+  let popular;
+
+  try {
+    popular = await getPopular();
+  } catch (errorMessage: any) {
+    if (errorMessage.toString() === 'Error: Administrator has disabled this endpoint.') {
+      popular = await getTrending();
+    } else {
+      error(500, errorMessage);
+    }
+  }
   return {
-    popular: await getPopular()
+    popular: popular
   };
 }

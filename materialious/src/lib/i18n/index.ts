bc932c468ae28bbdb2baf67ed1a9d29ac40c0f72
Ward <wardpearce@protonmail.com>
2024-04-27 15:44:08 +1200

    Added ru impor

--- materialious/src/lib/i18n/index.ts
+++ materialious/src/lib/i18n/index.ts
@@ -4,6 +4,7 @@ import { init, register } from 'svelte-i18n';
 const defaultLocale = 'en';
 
 register('en', () => import('./locales/en.json'));
+register('ru', () => import('./locales/ru.json'));
 
 init({
 	fallbackLocale: defaultLocale,

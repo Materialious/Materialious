2e6d1b35b42b0cb504b79a98035d5c5b51bedbaa
WardPearce <wardpearce@pm.me>
2024-09-10 16:50:42 +1200

    Serve ffmpeg locally

--- materialious/src/lib/videoDownload.ts
+++ materialious/src/lib/videoDownload.ts
@@ -138,12 +138,10 @@ export async function mergeMediaFromDASH(
     }
   });
 
-  const baseURL = 'https://unpkg.com/@ffmpeg/core-mt@0.12.6/dist/esm';
-
   await ffmpeg.load({
-    coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
-    wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
-    workerURL: await toBlobURL(`${baseURL}/ffmpeg-core.worker.js`, 'text/javascript')
+    coreURL: await toBlobURL('/ffmpeg/ffmpeg-core.js', 'text/javascript'),
+    wasmURL: await toBlobURL('/ffmpeg/ffmpeg-core.wasm', 'application/wasm'),
+    workerURL: await toBlobURL('/ffmpeg/ffmpeg-core.worker.js', 'text/javascript')
   });
 
   const { videoUrl, audioUrl } = selectedQuality;
--- materialious/vite.config.ts
+++ materialious/vite.config.ts
@@ -4,23 +4,9 @@ import { fileURLToPath, URL } from 'node:url';
 import { vite as vidstack } from 'vidstack/plugins';
 import { defineConfig } from 'vite';
 
-const viteServerConfig = {
-	name: "configure-response-headers",
-	configureServer(server) {
-		server.middlewares.use((req, res, next) => {
-			res.setHeader("Access-Control-Allow-Origin", "*");
-			res.setHeader("Access-Control-Allow-Methods", "GET");
-			res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-			res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-			next();
-		});
-	}
-};
-
 
 export default defineConfig({
 	plugins: [
-		viteServerConfig,
 		SvelteKitPWA({
 			injectRegister: 'inline',
 			manifest: {
@@ -56,14 +42,5 @@ export default defineConfig({
 	},
 	optimizeDeps: {
 		exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
-	},
-	server: {
-		headers: {
-			'Cross-Origin-Opener-Policy': 'same-origin',
-			'Cross-Origin-Embedder-Policy': 'require-corp'
-		},
-		fs: {
-			allow: ['../..']
-		}
 	}
 });

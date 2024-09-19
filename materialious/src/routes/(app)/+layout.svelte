37b8d6d7a9b7f9d24de6459d4f85e28549d38f3e
WardPearce <wardpearce@pm.me>
2024-09-19 13:57:52 +1200

    Start nodejs

--- materialious/src/routes/(app)/+layout.svelte
+++ materialious/src/routes/(app)/+layout.svelte
@@ -123,6 +123,30 @@
 		notifications = feed.notifications;
 	}
 
+	function channelListener(msg) {
+		console.log('[cordova] received:' + msg);
+	}
+
+	function startupCallback(err) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log('Node.js Mobile Engine Started');
+			nodejs.channel.send('Hello from Cordova!');
+		}
+	}
+
+	function startNodeProject() {
+		nodejs.channel.setListener(channelListener);
+		nodejs.start('main.js', startupCallback);
+		// To disable the stdout/stderr redirection to the Android logcat:
+		// nodejs.start('main.js', startupCallback, { redirectOutputToLogcat: false });
+	}
+
+	addEventListener('deviceready', () => {
+		startNodeProject();
+	});
+
 	onMount(async () => {
 		ui();
 

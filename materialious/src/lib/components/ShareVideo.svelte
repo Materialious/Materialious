bfcfe3bbb28e4550463fae24a66db353e601690a
WardPearce <wardpearce@pm.me>
2025-04-01 02:39:52 +1300

    Fixes to video sharing

--- materialious/src/lib/components/ShareVideo.svelte
+++ materialious/src/lib/components/ShareVideo.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+	import { instanceStore } from '$lib/store';
 	import { Clipboard } from '@capacitor/clipboard';
 	import { Capacitor } from '@capacitor/core';
 	import { _ } from 'svelte-i18n';
+	import { get } from 'svelte/store';
 	import type { Notification, PlaylistPageVideo, Video, VideoBase } from '../api/model';
 
 	interface Props {
@@ -11,28 +13,28 @@
 	let { video }: Props = $props();
 </script>
 
-{#if !Capacitor.isNativePlatform()}
-	<a
-		class="row"
-		href="#copy"
-		onclick={async () =>
-			await Clipboard.write({ string: `${location.origin}/watch/${video.videoId}` })}
-	>
-		<div class="min">{$_('player.share.materialiousLink')}</div></a
-	>
-{/if}
-<a
-	href="#copy"
+<button
+	class="row"
+	onclick={async () => {
+		if (Capacitor.isNativePlatform()) {
+			await Clipboard.write({ string: `${get(instanceStore)}/watch/${video.videoId}` });
+		} else {
+			await Clipboard.write({ string: `${location.origin}/watch/${video.videoId}` });
+		}
+	}}
+>
+	<div class="min">{$_('player.share.materialiousLink')}</div></button
+>
+<button
 	class="row"
 	onclick={async () =>
 		await Clipboard.write({ string: `https://redirect.invidious.io/watch?v=${video.videoId}` })}
 >
-	<div class="min">{$_('player.share.invidiousRedirect')}</div></a
-><a
+	<div class="min">{$_('player.share.invidiousRedirect')}</div></button
+><button
 	class="row"
-	href="#copy"
 	onclick={async () =>
 		await Clipboard.write({ string: `https://www.youtube.com/watch?v=${video.videoId}` })}
 >
-	<div class="min">{$_('player.share.youtubeLink')}</div></a
+	<div class="min">{$_('player.share.youtubeLink')}</div></button
 >

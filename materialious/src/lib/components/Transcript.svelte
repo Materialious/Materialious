12824a7255d2cab26542db3ae48906a9cf2571d7
WardPearce <wardpearce@pm.me>
2025-05-29 16:47:39 +1200

    Minor fix to transcript

--- materialious/src/lib/components/Transcript.svelte
+++ materialious/src/lib/components/Transcript.svelte
@@ -116,7 +116,7 @@
 						onclick={() => (playerElement.currentTime = cue.startTime)}
 						class:secondary-container={currentTime >= cue.startTime && currentTime <= cue.endTime}
 					>
-						<p class="chip no-margin no-padding">{videoLength(cue.startTime)}</p>
+						<p class="chip no-margin">{videoLength(cue.startTime)}</p>
 						<p class="transcript-text">{decodeHtmlCharCodes(cue.text.replace(/<[^>]+>/g, ''))}</p>
 					</div>
 				{/each}

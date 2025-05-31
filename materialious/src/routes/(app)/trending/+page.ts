f13480fabb2d3dd106f3f16d7d7f3efd830a9780
WardPearce <wardpearce@pm.me>
2025-05-31 15:38:46 +1200

    State used in trending and home page

--- materialious/src/routes/(app)/trending/+page.ts
+++ materialious/src/routes/(app)/trending/+page.ts
@@ -1,13 +1,21 @@
 import { getTrending } from '$lib/api/index';
-import type { Video } from '$lib/api/model';
+import { feedCacheStore } from '$lib/store';
 import { error } from '@sveltejs/kit';
+import { get } from 'svelte/store';
 
 export async function load() {
-	let trending: Video[];
-	try {
-		trending = await getTrending();
-	} catch (errorMessage: any) {
-		error(500, errorMessage);
+	let trending = get(feedCacheStore).trending;
+
+	if (!trending) {
+		try {
+			trending = await getTrending();
+		} catch (errorMessage: any) {
+			error(500, errorMessage);
+		}
+
+		feedCacheStore.set({ trending });
+	} else {
+		getTrending().then((newTrending) => feedCacheStore.set({ trending: newTrending }));
 	}
 
 	return { trending: trending };

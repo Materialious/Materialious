2cf211ebe154420f4d01acf2c8092d5a023e014e
ward <ward@localhost.localdomain>
2025-06-24 23:39:11 +1200

    Added loading indicator for tv routes on video change

---
+++ materialious/src/routes/(no-layout)/tv/[slug]/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import PageLoading from '$lib/components/PageLoading.svelte';
+	import { isTvRouteLoading } from '$lib/store';
+
+	let { children } = $props();
+</script>
+
+{#if $isTvRouteLoading}
+	<PageLoading />
+{:else}
+	{@render children?.()}
+{/if}

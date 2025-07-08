753195b58dc97b250568471a0095367ed02893ad
WardPearce <ward@localhost.localdomain>
2025-07-08 17:45:11 +1200

    Bump version to 1.9.19

--- materialious/src/routes/(no-layout)/tv/[slug]/+layout.svelte
+++ materialious/src/routes/(no-layout)/tv/[slug]/+layout.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
+	import { navigating } from '$app/stores';
 	import PageLoading from '$lib/components/PageLoading.svelte';
 	import { isTvRouteLoading } from '$lib/store';
 
 	let { children } = $props();
 </script>
 
-{#if $isTvRouteLoading}
+{#if $isTvRouteLoading || $navigating}
 	<PageLoading />
 {:else}
 	{@render children?.()}

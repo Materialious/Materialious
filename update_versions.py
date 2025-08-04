e2daaee88983390ab5a614a8f10ce25d0330cbeb
WardPearce <wardpearce@pm.me>
2025-08-04 17:51:12 +1200

    Bump version

--- update_versions.py
+++ update_versions.py
@@ -3,7 +3,7 @@ import os
 import re
 from datetime import datetime
 
-LATEST_VERSION = "1.9.23"
+LATEST_VERSION = "1.10.0"
 RELEASE_DATE = datetime.now().strftime("%Y-%-m-%d")  # Format: YYYY-M-D
 
 WORKING_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "materialious")

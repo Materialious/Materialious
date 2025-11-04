16b7d43321f16bf9a824c03c272858ec7abd6ea4
WardPearce <wardpearce@pm.me>
2025-11-05 04:52:26 +1300

    Subtitle fix for companion

--- materialious/src/lib/components/Player.svelte
+++ materialious/src/lib/components/Player.svelte
@@ -324,16 +324,11 @@
 
 			let dashUrl: string;
 
-			let usingCorsCompanionPatch =
-				import.meta.env.VITE_DEFAULT_COMPANION_INSTANCE && Capacitor.getPlatform() === 'web';
-			let usingCompanion = false;
-
 			// Due to CORs issues with redirects, hosted instances of Materialious
 			// dirctly provide the companion instance
 			// while clients can just use the reirect provided by Invidious' API
-			if (usingCorsCompanionPatch) {
+			if (import.meta.env.VITE_DEFAULT_COMPANION_INSTANCE) {
 				dashUrl = `${import.meta.env.VITE_DEFAULT_COMPANION_INSTANCE}/api/manifest/dash/id/${data.video.videoId}`;
-				usingCompanion = true;
 			} else {
 				if (!data.video.dashUrl) {
 					error(500, 'No dash manifest found');
@@ -343,19 +338,6 @@
 
 			if (!data.video.fallbackPatch && (!Capacitor.isNativePlatform() || $playerProxyVideosStore)) {
 				dashUrl += '?local=true';
-			}
-
-			if (!usingCorsCompanionPatch) {
-				const dashUrlObject = new URL(dashUrl);
-				if (!dashUrlObject.pathname.includes('companion')) {
-					dashUrlObject.pathname = `/companion${dashUrlObject.pathname}`;
-
-					const companionTestResp = await fetch(dashUrlObject, { method: 'HEAD' });
-					if (companionTestResp.ok) {
-						dashUrl = dashUrlObject.toString();
-						usingCompanion = true;
-					}
-				}
 			}
 
 			if (
@@ -370,14 +352,15 @@
 			}
 
 			if (data.video.captions) {
+				console.log(data.video.captions);
 				for (const caption of data.video.captions) {
 					let captionUrl: string;
-					if (!usingCorsCompanionPatch) {
+					if (!import.meta.env.VITE_DEFAULT_COMPANION_INSTANCE) {
 						captionUrl = caption.url.startsWith('http')
 							? caption.url
-							: `${get(instanceStore)}${usingCompanion ? '/companion' : ''}${caption.url}`;
+							: `${get(instanceStore)}${caption.url}`;
 					} else {
-						captionUrl = `${import.meta.env.VITE_DEFAULT_COMPANION_INSTANCE}/api/v1/captions/${data.video.videoId}`;
+						captionUrl = `${import.meta.env.VITE_DEFAULT_COMPANION_INSTANCE}${caption.url}`;
 					}
 
 					await player.addTextTrackAsync(
@@ -732,10 +715,15 @@
 		try {
 			await loadVideo();
 		} catch (error: unknown) {
-			if (!Capacitor.isNativePlatform() || data.video.fallbackPatch === 'youtubejs') return;
-			showVideoRetry = true;
+			if (
+				!Capacitor.isNativePlatform() ||
+				data.video.fallbackPatch === 'youtubejs' ||
+				(error as shaka.extern.Error).code !== 1001
+			)
+				return;
 
-			if ((error as shaka.extern.Error).code === 1001 && $playerYouTubeJsFallback) {
+			showVideoRetry = true;
+			if ($playerYouTubeJsFallback) {
 				await reloadVideo();
 			}
 		}

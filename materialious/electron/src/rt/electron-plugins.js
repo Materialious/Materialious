b1ae205e381b0e97c504f1ae1e3964a23512c225
WardPearce <wardpearce@pm.me>
2026-09-10 20:47:47 +1200

    Update electron-plugins.js

--- materialious/electron/src/rt/electron-plugins.js
+++ materialious/electron/src/rt/electron-plugins.js
@@ -1,6 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const CapacitorNodejs = require('../../../node_modules/capacitor-nodejs/electron/dist/plugin.js');
 
 module.exports = {
-  CapacitorNodejs,
 }
